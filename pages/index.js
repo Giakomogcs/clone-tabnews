868fa18f389d6d64156b2c5b3efc426e0505e86e
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2023-09-29 01:33:32 +0000

    Adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Só vejo isso como motivos para rir depois 💫</h1>
+function Home() {
+  return <h1>Só vejo isso como motivos para rir depois 💫</h1>;
 }
 
 export default Home;
+
+function gigi() {
+  gigilinfo();
+}
