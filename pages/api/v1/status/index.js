c95f0cf41ad2173b388061b2d91f0df4450d0030
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-01-04 00:53:49 +0000

    database get status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Você é uma pessoa acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  // Consulta para obter informações do PostgreSQL
+  const postgresInfoQuery =
+    "SELECT version() as version, setting as max_connections FROM pg_settings WHERE name = 'max_connections'";
+  const postgresInfoResult = await database.query(postgresInfoQuery);
+
+  // Extrai informações do resultado da consulta
+  const fullPostgresVersion = postgresInfoResult.rows[0].version;
+  const versionMatches = fullPostgresVersion.match(/(\d+\.\d+\.\d+)/);
+  const postgresVersion = versionMatches
+    ? versionMatches[0]
+    : "Versão não encontrada";
+
+  const maxConnections = postgresInfoResult.rows[0].max_connections;
+
+  // Consulta para obter o número de conexões atuais
+  const currentConnectionsQuery =
+    "SELECT COUNT(*) as current_connections FROM pg_stat_activity";
+  const currentConnectionsResult = await database.query(
+    currentConnectionsQuery,
+  );
+  const currentConnections =
+    currentConnectionsResult.rows[0].current_connections;
+
+  const queries = {
+    updated_at: updatedAt,
+    postgres_version: postgresVersion,
+    max_connections: maxConnections,
+    current_connections: currentConnections,
+    // Adicione mais informações conforme necessário
+  };
+
+  response.status(200).json(queries);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Lê o corpo da resposta como texto
+  const responseBodyString = await response.text();
+  console.log(JSON.stringify(JSON.parse(responseBodyString), null, 2));
+
+  // Utiliza o corpo da resposta como JSON
+  const responseBody = JSON.parse(responseBodyString);
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
