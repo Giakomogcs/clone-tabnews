6b55a843afe3535a68a00c9fba3b644771a20141
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-01-04 23:28:00 +0000

    max connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,38 +3,24 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  // Consulta para obter informações do PostgreSQL
-  const postgresInfoQuery =
-    "SELECT version() as version, setting as max_connections FROM pg_settings WHERE name = 'max_connections'";
-  const postgresInfoResult = await database.query(postgresInfoQuery);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  // Extrai informações do resultado da consulta
-  const fullPostgresVersion = postgresInfoResult.rows[0].version;
-  const versionMatches = fullPostgresVersion.match(/(\d+\.\d+\.\d+)/);
-  const postgresVersion = versionMatches
-    ? versionMatches[0]
-    : "Versão não encontrada";
-
-  const maxConnections = postgresInfoResult.rows[0].max_connections;
-
-  // Consulta para obter o número de conexões atuais
-  const currentConnectionsQuery =
-    "SELECT COUNT(*) as current_connections FROM pg_stat_activity";
-  const currentConnectionsResult = await database.query(
-    currentConnectionsQuery,
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
-  const currentConnections =
-    currentConnectionsResult.rows[0].current_connections;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const queries = {
+  response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion,
-    max_connections: maxConnections,
-    current_connections: currentConnections,
-    // Adicione mais informações conforme necessário
-  };
-
-  response.status(200).json(queries);
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,15 @@ test("GET to /api/v1/status should return 200", async () => {
 
   // Lê o corpo da resposta como texto
   const responseBodyString = await response.text();
-  console.log(JSON.stringify(JSON.parse(responseBodyString), null, 2));
 
   // Utiliza o corpo da resposta como JSON
   const responseBody = JSON.parse(responseBodyString);
 
   expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.postgres_version).toBeDefined();
-  expect(responseBody.max_connections).toBeDefined();
-  expect(responseBody.current_connections).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
