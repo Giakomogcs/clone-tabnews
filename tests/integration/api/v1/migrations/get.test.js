cf233c6fce6a8841d3d022f6f481f1c322f8f5a1
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-04-03 00:51:48 +0000

    jest configuration

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from "infra/database";
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -6,4 +8,9 @@ test("GET to /api/v1/migrations should return 200", async () => {
   console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  //expect(responseBody.length).toBeGreaterThan(0);
+
+  console.log(process.env.NODE_ENV); //provando que as variáveis de ambiente são .env.test
+
+  expect(process.env.POSTGRES_HOST).toBe("localhost");
 });
