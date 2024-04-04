02ca547b4792af40952c3169877759ab1b88f804
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-04-04 00:25:20 +0000

    refatoração e testes migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,5 +1,11 @@
 import database from "infra/database";
 
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -8,9 +14,9 @@ test("GET to /api/v1/migrations should return 200", async () => {
   console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
-  //expect(responseBody.length).toBeGreaterThan(0);
 
-  console.log(process.env.NODE_ENV); //provando que as variáveis de ambiente são .env.test
+  expect(responseBody.length).toBeGreaterThan(0);
+  console.log("NODE_ENV: " + process.env.NODE_ENV); //provando que as variáveis de ambiente são .env.test
 
   expect(process.env.POSTGRES_HOST).toBe("localhost");
 });
