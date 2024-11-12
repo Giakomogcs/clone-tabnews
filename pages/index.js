5d68c742ee47708fe782fdb3ec1a5594cfcfa17a
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-11-12 01:16:26 +0000

    changing `detail` of page

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ function Home() {
       <h1>Só vejo isso como motivos para rir depois 💫</h1>
       <br />
       <p>
-        A vida só vai fazer realmente sentido quando o tempo passar e você ölhar
-        para trás" e juntar todos os pontos. Assim lembrar dos problemas,
+        A vida só vai fazer realmente sentido quando o tempo passar e você
+        "olhar para trás" e juntar todos os pontos. Assim lembrar dos problemas,
         Momentos dificeis, aprendizados e sonhos realizados. E rir, pois nada
         nesse mundo é por acaso e tudo no final será motivos para rir depois!
       </p>
