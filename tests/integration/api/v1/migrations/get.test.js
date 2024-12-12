24cad8f79590b2542de41d740171b81016c5a554
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2024-12-12 00:06:41 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js` > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest -k -s command-jest 'next dev' --hide next 'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
    🔴 Aguardando Postgres aceitar conexões🟢 Postgre está pronto e aceitando conexões!
    [jest] info  - Loaded env from /workspaces/clone-tabnews/.env.test
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.597 s, estimated 2 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. more robust with  and

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,22 +1,17 @@
-import database from "infra/database";
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
-
   expect(responseBody.length).toBeGreaterThan(0);
-  console.log("NODE_ENV: " + process.env.NODE_ENV); //provando que as variáveis de ambiente são .env.test
-
-  expect(process.env.POSTGRES_HOST).toBe("localhost");
 });
