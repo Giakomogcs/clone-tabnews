dd9c5081272581075ac3ee8ec6880b8678841376
Giovani Costa Silva <87100712+Giakomogcs@users.noreply.github.com>
2025-03-19 23:04:04 +0000

    feat(\): create a rule to validate e-mail

--- models/user.js
+++ models/user.js
@@ -1,6 +1,33 @@
 import database from "infra/database";
+import { ValidationError } from "infra/errors";
 
 async function create(userIputValues) {
+  await validadeUniqueEmail(userIputValues.email);
+  const newUser = await runInsertQuery(userIputValues);
+  return newUser;
+}
+async function validadeUniqueEmail(email) {
+  const results = await database.query({
+    text: `
+      SELECT 
+        email
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      ;`,
+    values: [email],
+  });
+
+  if (results.rowCount > 0) {
+    throw new ValidationError({
+      message: "O email informado já está sendo utilizado",
+      action: "Ultilize outo email para realizar o cadastro",
+    });
+  }
+}
+
+async function runInsertQuery(userIputValues) {
   const results = await database.query({
     text: `
       INSERT INTO 
